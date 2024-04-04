fa442d24aa4ad5e012aac2d0347b33f7eb4a034d
saurabh1605 <sv439a@gmail.com>
2024-04-04 13:47:13 +0530

    project add

---
+++ src/main/java/com/card/validation/cardValid/CardValidApplication.java
@@ -0,0 +1,13 @@
+package com.card.validation.cardValid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardValidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardValidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/card/validation/cardValid/CardValidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card.validation.cardValid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardValidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
